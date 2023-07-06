2d4ad2bbe263b8e752b868d7203a00ea5ebcb478
cumauu73 <cumauu73@icloud.com>
2023-07-06 06:50:50 -0500

    pseudo

---
+++ game.js
@@ -0,0 +1,40 @@
+//varaibles
+    //num wins
+    //num lose
+    //num ties
+
+
+//algorith
+
+//pseudo-code
+
+//human player 
+
+//computer player
+
+//player chosie
+
+//computer randomly 
+
+//win lose
+
+//compare
+
+// if human is s and computer p or
+// if human is r and computer s or
+//if human is p and computer r
+//win 
+//increase win
+
+//if computerchoice===human
+//ties
+//increase tise
+
+//else
+//lost
+//increase loses
+
+//ask user douwanna paly again
+//if yes restart game
+//else
+//exit game
