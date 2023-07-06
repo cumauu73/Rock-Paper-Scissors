7f3864fae2d631644e0d0f7fca5f256032495d4d
cumauu73 <cumauu73@icloud.com>
2023-07-06 11:33:04 -0500

    algorithm

--- game.js
+++ game.js
@@ -1,83 +1,38 @@
 var wins = 0;
-var losse = 0;
+var losses = 0;
 var ties = 0;
-
-
-var choices = ["R", "P" ,"S"]
-//human choice- get choice from usefr input
-var humanChoice = prompt("Choose R, P or S")
-
-
-
-
-//computer choice
-var randomNum = Math.floor(Math.random() *3)
-var computerChoice = choices[randomNum]
-
-console.log("Humancho :" + humanChoice)
-console.log("Computer :" + computerChoice)
-
-if ( 
-    humanChoice === "S" && computerChoice === "P" ||
-    humanChoice === "R" && computerChoice === "S" ||
-    humanChoice === "P" && computerChoice === "R"
-    // if human is s and computer p or
-// if human is r and computer s or
-//if human is p and computer r
-//win 
-
-) {
-    //alert we win
-    alert("You won!")
-    //increase win
-
-} else if (
-    humanChoice===computerChoice
-    //if computerchoice===human
-) {
-    alert("Round tied!")
- //alert ties
-//ties
-//increase tise
-} else {
-//else
-//alert loss
-alert("You lost!")
-//lost
-//increase loses
+var choices = ["R", "P", "S"]
+var keepPlaying = true
+while (keepPlaying) {
+    var humanChoice = prompt("Choose R, P or S")
+    if (humanChoice) {
+        humanChoice = humanChoice.toUpperCase()
+        if (!choices.includes(humanChoice)) {
+            alert("You must choose R, P or S")
+            continuer
+        }
+        var randomNum = Math.floor(Math.random() * 3)
+        var computerChoice = choices[randomNum]
+        console.log("Humancho :" + humanChoice)
+        console.log("Computer :" + computerChoice)
+        if (
+            humanChoice === "S" && computerChoice === "P" ||
+            humanChoice === "R" && computerChoice === "S" ||
+            humanChoice === "P" && computerChoice === "R"
+        ) {
+            alert("You won!")
+            wins++
+        } else if (
+            humanChoice === computerChoice
+        ) {
+            alert("Round tied!")
+            ties++
+        } else {
+            alert("You lost!")
+            losses++
+        }
+        alert("wins: " + wins + "\nLosses: " + losses + "\nTies: " + ties)
+        keepPlaying = confirm("Do you want to play again?")
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// alert,  confirm,  prompt
-//algorith
-
-//pseudo-code
-
-//human player 
-
-//computer player
-
-//player chosie
-
-//computer randomly 
-
-//win lose
-
-//compare
-
-
-//ask user douwanna paly again
-//if yes restart game
-//else
-//exit game
+alert("Seeya. Play again soon!")
